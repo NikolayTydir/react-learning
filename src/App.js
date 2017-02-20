67341b838ca8f1310b5e75fc5ddc1d455779d990
NikolayTydir <vputth1@gmail.com>
2017-02-20 10:59:56 +0200

    Add dropdownText

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import  React, { Component } from  'react';
-
+import Dropdown from './Dropdown';
 
 class App extends  Component {
     render() {
-        return <div>Hello world</div>;
-    }
+        return(
+            <div>
+                <Dropdown/>
+            </div>
+        );
+     }
 
-}
+    }
 
 export  default App;
---
+++ src/Dropdown.js
@@ -0,0 +1,27 @@
+import  React, { Component } from  'react';
+
+
+class Dropdown extends  Component {
+    constructor(props){
+        super(props);
+        this.state = {isOpened: false};
+    }
+    toggleState(){
+        this.setState({isOpened: !this.state.isOpened});
+    }
+    render() {
+        console.log('isOpened', this.state.isOpened);
+        let dropdownText;
+        if (this.state.isOpened){
+            dropdownText= <div> here is what is show in drop down</div>
+        }
+        return (
+            <div onClick={this.toggleState.bind(this)}>
+                its dropdown baby
+                {dropdownText}
+            </div>);
+    }
+
+}
+
+export  default Dropdown;
